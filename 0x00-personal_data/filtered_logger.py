ee5bcdcc1e133a549f956f5df20a4a6d9a0e6258
Kidus Gizaw <50879166+KidusGR@users.noreply.github.com>
2023-09-22 23:05:43 +0300

    0x00-personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ import re
 from typing import List
 import mysql.connector
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+PII_FIELDS: tuple = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -17,17 +17,20 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """ Replacing """
 
     for f in fields:
-        message = re.sub(rf"{f}=(.*?)\{separator}",
-                         f'{f}={redaction}{separator}', message)
+        message: str = re.sub(
+            rf"{f}=(.*?)\{separator}",
+            f'{f}={redaction}{separator}',
+            message)
     return message
 
 
 class RedactingFormatter(logging.Formatter):
     """ RedactingFormatter class. """
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    REDACTION: str = "***"
+    FORMAT: str = "[HOLBERTON] %(name)s %(levelname)s %\
+(asctime)-15s: %(message)s"
+    SEPARATOR: str = ";"
 
     def __init__(self, fields: List[str]) -> None:
         """ Initialization of class """
@@ -45,10 +48,10 @@ class RedactingFormatter(logging.Formatter):
 def get_logger() -> logging.Logger:
     """ A method that implements a logger """
 
-    logger = logging.getLogger("user_data")
+    logger: logging.Logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    handler = logging.StreamHandler()
+    handler: logging.StreamHandler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(handler)
     return logger
@@ -57,11 +60,11 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ A method that implements db conectivity """
 
-    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
-    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(
+    psw: str = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username: str = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    host: str = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name: str = os.environ.get('PERSONAL_DATA_DB_NAME')
+    conn: mysql.connector.connection.MySQLConnection = mysql.connector.connect(
         host=host,
         database=db_name,
         user=username,
@@ -72,11 +75,11 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
 def main() -> None:
     """ A method that implements a main function """
 
-    db = get_db()
-    cursor = db.cursor()
+    db: mysql.connector.connection.MySQLConnection = get_db()
+    cursor: mysql.connector.cursor.MySQLCursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
     for row in cursor:
-        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+        message: str = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
             f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
             f"last_login={row[6]}; user_agent={row[7]};"
         print(message)
